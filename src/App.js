7cf7bea01cbc9b739b5668f313b88c962b64bb8c
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-02-26 11:37:32 -0300

    Criando a pagina de componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-     <h1>Ola MUNDO</h1>
+      <Navbar />
     </div>
   );
 }
